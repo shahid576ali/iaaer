46cd551721813ff8ec755d5a311c285e5d88413b
Subhash Kumar <subhashkushwah134@gmail.com>
2025-01-30 21:06:29 +0530

    added code

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { AboutSection } from "./components/about/AboutSection"
+import { HighlightsSection } from "./components/highlights/HighlightsSection"
+import Navbar from "./components/nav/Navbar"
+import { Sidebar } from "./components/sidebar/Sidebar"
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+
+      <main className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
+          <div className="lg:col-span-2 lg:ml-[100px] space-y-12">
+            <AboutSection />
+            <HighlightsSection />
+          </div>
+          <Sidebar />
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ assets/images.jsx
@@ -0,0 +1,11 @@
+import linkedin from './LinkedIn.png'
+import kpmg from './kpmg.jpeg'
+import acca from './acca.jpeg'
+
+const images = {
+    linkedin,
+    kpmg,
+    acca
+}
+
+export default images
