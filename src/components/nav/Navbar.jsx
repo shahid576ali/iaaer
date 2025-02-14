e888d680b05ae72f5c006fe5a7f96a0679c7a5b8
Subhash Kumar <subhashkushwah134@gmail.com>
2025-02-14 20:59:24 +0530

    Update Navbar.jsx

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -113,7 +113,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gradient-to-r from-purple-400 to-purple-600 fixed top-0 left-0 right-0 z-50 overflow-auto" ref={navRef}>
+    <nav className="bg-gradient-to-r from-purple-400 to-purple-600 fixed top-0 left-0 right-0 z-50" ref={navRef}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
