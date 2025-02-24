614e86bab80bab4dec2cd44f5714b577d142cb8f
Subhash Kumar <subhashkushwah134@gmail.com>
2025-02-24 21:20:39 +0530

    Update Card.jsx

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,7 +1,7 @@
 function Card({ children, className = "" }) {
     return (
       <div
-        className={`bg-white rounded-lg shadow-sm border flex flex-row-reverse border-gray-200 overflow-hidden transition-shadow hover:shadow-md ${className}`}
+        className={`bg-white rounded-lg shadow-sm border sm:flex sm:flex-row-reverse border-gray-200 overflow-hidden transition-shadow hover:shadow-md ${className}`}
       >
         {children}
       </div>
