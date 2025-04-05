655f8baec01fd33223e5c72c42bf73319b687f3a
Sanyamdksh <sanyamdaksh19@gmail.com>
2025-04-05 18:55:56 +0530

    Research Folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import Footer from "./components/footer/Footer";
 import Navbar from "./components/nav/Navbar";
 import Home from "./pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
-import Current from './pages/News/Current/Current'
-import Archive from './pages/News/Archive/Archive'
+import Current from "./pages/News/Current/Current";
+import Archive from "./pages/News/Archive/Archive";
 import Newsletter from "./pages/News/Newsletters/NewsLetter";
-
+import Affiliated from "./pages/Research/Affiliated/affiliated";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
-        <main className="max-w-full lg:mr-28 mx-auto px-4 sm:px-6 lg:px-8 py-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/news/current" element={<Current />} />
-            <Route path="/news/newsletters" element={<Newsletter />} />
-            <Route path="/news/archive" element={<Archive />} />
-          </Routes>
-        </main>
+      <main className="max-w-full lg:mr-28 mx-auto px-4 sm:px-6 lg:px-8 py-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news/current" element={<Current />} />
+          <Route path="/news/newsletters" element={<Newsletter />} />
+          <Route path="/news/archive" element={<Archive />} />
+          <Route
+            path="/research/affiliated-journals"
+            element={<Affiliated />}
+          />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
